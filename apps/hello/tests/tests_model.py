5a546cb0f8c3eee1688747e9d65fc5b2083f9245
Octo <octo.oleg@gmail.com>
2017-05-25 07:37:45 +0300

    ticket 1: Test for model

---
+++ apps/hello/tests/tests_model.py
@@ -0,0 +1,24 @@
+from django.test import TestCase
+from apps.hello.models import Contact
+from model_mommy import mommy
+
+# Create your tests here.
+
+
+class SomeTestsModel(TestCase):
+
+    def setUp(self):
+        self.my_instance = mommy.make('hello.Contact',id=2)
+
+    def test_the_model(self):
+        """Test the model Contact"""
+
+        first_query = Contact.objects.get(id=2)
+        self.assertEqual(first_query.name, self.my_instance.name)
+        self.assertEqual(first_query.surname, self.my_instance.surname)
+        self.assertEqual(first_query.bio, self.my_instance.bio)
+        self.assertEqual(first_query.email, self.my_instance.email)
+        self.assertEqual(first_query.jabber, self.my_instance.jabber)
+        self.assertEqual(first_query.date, self.my_instance.date)
+        self.assertEqual(first_query.skype, self.my_instance.skype)
+        self.assertEqual(first_query.contacts, self.my_instance.contacts)
