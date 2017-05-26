165baf4ba3db77c5a713b18c5f2ef95cf99e6816
Octo <octo.oleg@gmail.com>
2017-05-26 23:20:19 +0300

    ticket 1:Upgrade test

--- apps/hello/tests/tests_model.py
+++ apps/hello/tests/tests_model.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from django.test import TestCase
 from apps.hello.models import Contact
 from model_mommy import mommy
@@ -8,7 +9,7 @@ from model_mommy import mommy
 class SomeTestsModel(TestCase):
 
     def setUp(self):
-        self.my_instance = mommy.make('hello.Contact', id=2)
+        self.my_instance = mommy.make('hello.Contact', id=2, name=u'Олег', surname=u'Панчишин')
 
     def test_the_model(self):
         """Test the model Contact"""
@@ -26,5 +27,12 @@ class SomeTestsModel(TestCase):
     def test_that_data_base_is_not_empty(self):
         """ Test that data base is not empty first objects loads from fixtures and second I made with model_mommy"""
 
+        object_list = Contact.objects.all()
+        self.assertTrue(object_list)  # Test that database list is not empty
         self.assertIsNot(Contact.objects.all().count(), 0)
-        self.assertEqual(Contact.objects.all().count(), 2)
+        self.assertEqual(Contact.objects.all().count(), 2)  # Test that base data has saved 2 objects
+
+    def test_the_unicode_in_base(self):
+        """Test in case unicode in base data """
+
+        self.assertEqual(self.my_instance.__unicode__(), u'Олег Панчишин')
